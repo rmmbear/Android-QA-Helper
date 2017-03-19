fca40c4b2e0e9dda55e78da3c8bad55241a1d0fa
rmmbear <rmmbear@users.noreply.github.com>
2017-03-19 20:33:40 +0000

    Add windows batch file shortcuts for helper commands
    These have been included in the windows frozen code package, but
    not in the repository itself.

---
+++ windows_shortcuts/Pull traces.bat
@@ -0,0 +1,13 @@
+@echo off
+
+title Helper - Quick pull traces
+
+set _root=%~dp0
+set root=%_root:~0,-1%
+set helper="%root%\helper\helper.exe"
+
+echo.
+call %helper% -t %userprofile%\Desktop
+echo.
+
+pause
