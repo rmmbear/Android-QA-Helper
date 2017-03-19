2116670f911d27f64e0f4e27942ac8c3b1df777f
rmmbear <maciej.my.yk@gmail.com>
2017-03-19 20:33:40 +0000

    Add windows batch file shortcuts for helper commands
    These have been included in the windows frozen code package, but
    not in the repository itself.

---
+++ windows_shortcuts/Helper_console.bat
@@ -0,0 +1,6 @@
+@echo off
+
+title Helper Console - Type 'helper -h' for help!
+
+echo.
+cmd /K echo You are now in the helper's directory and can issue commands to it. See 'helper -h' for available options and help. ^& cd helper ^& prompt $G$G$G
