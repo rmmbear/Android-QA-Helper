1249bdbbb798dae8e4de06d0eccd7df087667147
rmmbear <maciej.my.yk@gmail.com>
2017-03-29 23:21:18 +0200

    Refactor test structure

--- helper/tests/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Launch this module (python -m helper.tests) to automatically perform all tests.
-"""
-import pytest
-
-pytest.main()
--- helper/tests/test_pytest.py
+++ helper/tests/test_pytest.py
@@ -1,76 +1,22 @@
 import shutil
 from pathlib import Path
-from collections import OrderedDict
 
-import helper as _helper
-from helper import main as _main
+import helper as helper_
+import helper.main as main_
+import helper.tests as tests_
 
 
-FULL_DEVICE_CONFIG = _helper.BASE + "/tests/full_config"
-DEVICE_CONFIG = {
-    "ls /system/bin"                        :"available_commands",
-    "cat /proc/meminfo"                     :"meminfo",
-    "wm size"                               :"screen_size",
-    "wm density"                            :"screen_density",
-    "getprop"                               :"getprop",
-    "cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq" :"cpu_freq",
-    "cat /proc/cpuinfo"                     :"cpuinfo",
-    "cat /sys/devices/system/cpu/present"   :"cpu_cores",
-    "dumpsys SurfaceFlinger"                :"surfaceflinger_dump"
-    }
+FULL_DEVICE_CONFIG = tests_.FULL_DEVICE_CONFIG
+DEVICE_CONFIG = tests_.DEVICE_CONFIG
 
-GARBAGE = "".join([chr(x) for x in range(1, 0x110000) if chr(x).isprintable()])
-
-class DummyDevice(_main.Device):
+class DummyDevice(main_.Device):
 
 
     def __init__(self, config_dir, serial=999999, status="device"):
-
-        self.serial = serial
         self.config_dir = Path(config_dir)
+        self.ext_storeage = None
 
-        self.anr_trace_path = None
-        self.available_commands = None
-        self.info = OrderedDict()
-
-        info = [
-            ("Product", ["Model",
-                         "Name",
-                         "Manufacturer",
-                         "Brand",
-                         "Device"]),
-            ("OS",      ["Android Version",
-                         "Android API Level",
-                         "Build ID",
-                         "Build Fingerprint"]),
-            ("RAM",     ["Total"]),
-            ("CPU",     ["Chipset",
-                         "Processor",
-                         "Cores",
-                         "Architecture",
-                         "Max Frequency",
-                         "Available ABIs"]),
-            ("GPU",     ["Model",
-                         "GL Version",
-                         "Compression Types"]),
-            ("Display", ["Resolution",
-                         "Refresh-rate",
-                         "V-Sync",
-                         "Soft V-Sync",
-                         "Density",
-                         "X-DPI",
-                         "Y-DPI"])
-            ]
-
-        for pair in info:
-            props = OrderedDict()
-
-            for prop in pair[1]:
-                props[prop] = None
-
-            self.info[pair[0]] = props
-
-        self.initialized = False
+        main_.Device.__init__(self, serial)
 
         if not status:
             self.status = "device"
@@ -119,10 +65,7 @@ class TestDeviceInit:
     def test_empty(self):
         """Test device initialization without any actual input.
         """
-        an_unlikely_directory = "/a/b/c/d/f/v/g/h/n/v/sd/a"
-        # TODO: generate an unlikely directory instead of having it hard-coded
-
-        device = DummyDevice(an_unlikely_directory)
+        device = DummyDevice(tests_.get_nonexistent_path)
 
         assert device.initialized
         assert not device.available_commands
@@ -140,14 +83,15 @@ class TestDeviceInit:
         """Test device initialization with garbage unicode input for all
         device's info gathering methods.
         """
-        tmp = Path(_helper.BASE + "/tests/garbage_config")
+        tmp = Path(helper_.BASE + "/tests/garbage_config")
         tmp.mkdir(exist_ok=True)
 
         config_filenames = list(DEVICE_CONFIG.values())
 
         garbage = tmp / config_filenames[0]
         with garbage.open(mode="w", encoding="utf-8") as eff_me_up:
-            eff_me_up.write(GARBAGE)
+            eff_me_up.write(
+         "".join([chr(x) for x in range(1, 0x110000) if chr(x).isprintable()]))
 
         for filename in config_filenames[1::]:
             shutil.copy(str(garbage), str(tmp / filename))
@@ -196,7 +140,7 @@ class TestDeviceInit:
         unicode garbage, meaning that all attributes will be filled with
         meaningless unicode string.
         """
-        tmp = Path(_helper.BASE + "/tests/garbage_full_config")
+        tmp = Path(helper_.BASE + "/tests/garbage_full_config")
         tmp.mkdir(exist_ok=True)
 
         for filename in DEVICE_CONFIG.values():
@@ -222,12 +166,16 @@ class TestDeviceInit:
                       "[ro.build.version.sdk]",
                       "[ro.build.id]",
                       "[ro.build.fingerprint]",
-                      "[ro.sf.lcd_density]"]
+                      "[ro.sf.lcd_density]"
+                     ]
 
+        garbage_ = "".join(
+            [chr(x) for x in range(1, 0x110000) if chr(x).isprintable()])
         with (tmp / "getprop").open(mode="w", encoding="utf-8") as getprop_file:
+
             for prop in prop_names:
                 getprop_file.write(prop + " : [")
-                getprop_file.write(GARBAGE)
+                getprop_file.write(garbage_)
                 getprop_file.write("]\n")
 
         device = DummyDevice(tmp)
@@ -246,15 +194,14 @@ class TestDeviceInit:
         return device
 
 
-class TestADB:
-
+class TestNonexistentBinaries:
     def test_no_adb(self):
         """Make sure the program stops when there is no adb binary.
         """
-        _main.ADB = "/just/some/nonsense"
+        main_.ADB = tests_.get_nonexistent_path()
 
         try:
-            _main.adb_execute("")
+            main_.adb_execute("")
         except SystemExit:
             assert True
             return True
@@ -264,41 +211,12 @@ class TestADB:
     def test_no_aapt(self):
         """Make sure the program stops when there is no aapt binary.
         """
-        _main.AAPT = "/just/some/nonsense"
+        main_.AAPT = tests_.get_nonexistent_path()
 
         try:
-            _main.aapt_execute("")
+            main_.aapt_execute("")
         except SystemExit:
             assert True
             return True
 
         assert False
-
-
-def dump_devices(directory):
-    """Function for dumping device information used in device initiation. Dumped
-    files can be used with TestDeviceInit.test_full().
-    """
-    print("Before continuing, please remember that ALL dumped files may contain",
-          "sensitive data. Please pay special attention to the 'getprop' file",
-          "which almost certainly will contain data you do not want people to",
-          "see.",)
-    input("Press enter to continue")
-
-    Path(directory).mkdir(exist_ok=True)
-
-    for device in _main.get_devices():
-        device_id = device.info["Product"]["Model"] + "_" + device.info["Product"]["Manufacturer"]
-        device_dir = Path(directory, device_id + "_DUMP")
-        device_dir.mkdir(exist_ok=True)
-        print()
-        print("Dumping", device_id)
-
-        for command, filename in DEVICE_CONFIG.items():
-            output = device.shell_command(command, return_output=True,
-                                          as_list=False)
-
-            with Path(device_dir, filename).open(mode="w", encoding="utf-8") as dump_file:
-                dump_file.write(output)
-
-        print("Device dumped to", str(device_dir.resolve()))
