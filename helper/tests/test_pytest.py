f4609683cf762c6660e5019e76b1aa9344a1a744
rmmbear <rmmbear@users.noreply.github.com>
2017-09-24 20:34:19 +0200

    Fix tests

--- helper/tests/test_pytest.py
+++ helper/tests/test_pytest.py
@@ -11,20 +11,38 @@ COMPATIBILITY_OUT_DIR = helper_.BASE + "/tests/compatibility_output"
 
 
 class DummyDevice(device_.Device):
-    def _device_init(self, config_dir, ignore_nonexistent_files=False):
-        """Gather all the information."""
+    def __init__(self, *args, config_dir=None, ignore_nonexistent_files=False,
+                 **kwargs):
+
+        self.config_dir = config_dir
+        self.ignore_nonexistent_files = ignore_nonexistent_files
+
+        super().__init__(*args, **kwargs)
+
+
+    def device_init(self, limit_init=(), force_init=False,
+                    ignore_nonexistent_files=None):
+
+        if not ignore_nonexistent_files:
+            ignore_nonexistent_files = self.ignore_nonexistent_files
+
         for info_source, info_specs in device_.INFO_EXTRACTION_CONFIG.items():
-            if self.limit_init and info_source[-1] not in self.limit_init:
+            if limit_init and info_source[-1] not in limit_init:
+                print(info_source[-1], "not in", limit_init)
                 continue
+            print(info_source[-1], "found in", limit_init)
+            print("continuing with", info_source[-1])
 
             try:
                 kwargs = dict(info_source[1])
             except IndexError:
                 kwargs = {}
 
-            source_file = Path(config_dir) / info_source[-1]
+            source_file = Path(self.config_dir) / info_source[-1]
             source_output = ''
+
             print("Reading file:", str(source_file))
+
             try:
                 with source_file.open(mode="r", encoding="utf-8") as f:
                     source_output = f.read()
@@ -49,16 +67,16 @@ class TestDeviceInit:
         """Test device initialization with a complete input from an actual
         device.
         """
-        device = DummyDevice(config_dir, 'dummy')
-        device._device_init(config_dir, ignore_nonexistent_files=ignore_nonexistent_files)
+        device = DummyDevice("dummy_full", config_dir=config_dir,
+                             ignore_nonexistent_files=ignore_nonexistent_files)
 
-        device.print_full_info()
+        print(device.full_info_string(initialize=False))
         if write_output:
             device_file = "".join(["/", device.info('Product', 'Manufacturer'),
                                    "_", device.info('Product', 'Model')])
             write_output += device_file
             with open(write_output, mode='w', encoding='utf-8') as output_file:
-                output_file.write(device.get_full_info_string())
+                output_file.write(device.full_info_string())
                 output_file.write("\n\n\n")
                 for key, value in device.__dict__.items():
                     output_file.write("".join([str(key), " : ", str(value)]))
@@ -94,10 +112,10 @@ class TestDeviceInit:
 
     def test_all_limited(self, config_dir=FULL_DEVICE_CONFIG):
         """"""
-        device = DummyDevice(1, 'dummy', limit_init=['nonexistent_info_source'])
-        device._device_init(config_dir)
+        device = DummyDevice("dummy_fill_limited", limit_init=('asssss',),
+                             config_dir=config_dir)
 
-        device.print_full_info()
+        print(device.full_info_string(initialize=False))
 
         assert not device.available_commands
         assert not device.anr_trace_path
@@ -111,10 +129,10 @@ class TestDeviceInit:
         """Test device initialization without any actual input.
         """
         random_dir = tests_.get_nonexistent_path()
-        device = DummyDevice(2, 'dummy')
-        device._device_init(random_dir, True)
+        device = DummyDevice("dummy_empty", config_dir=random_dir,
+                             ignore_nonexistent_files=True)
 
-        device.print_full_info()
+        print(device.full_info_string(initialize=False))
 
         assert not device.available_commands
         assert not device.anr_trace_path
@@ -122,104 +140,3 @@ class TestDeviceInit:
         for category in device._info.values():
             for key, value in category.items():
                 assert key and not value
-
-
-    '''
-    def test_garbage(self):
-        """Test device initialization with garbage unicode input for all
-        device's info gathering methods.
-        """
-        tmp = Path(helper_.BASE + "/tests/garbage_config")
-        tmp.mkdir(exist_ok=True)
-
-        garbage = tmp / 'garbage_file'
-        with garbage.open(mode="w", encoding="utf-8") as eff_me_up:
-            eff_me_up.write("".join([chr(x) for x in range(1, 0x110000) if chr(x).isprintable()]))
-
-        for info_source in device_.INFO_EXTRACTION_CONFIG:
-            filename = info_source[-1]
-            shutil.copy(str(garbage), str(tmp / filename))
-
-        device = DummyDevice(1234567890)
-        device._device_init(tmp)
-
-        #for config_file in tmp.iterdir():
-        #    config_file.unlink()
-        #tmp.rmdir()
-
-        assert device.available_commands
-        assert not device.anr_trace_path
-
-        for category in device._info.values():
-            for key, value in category.items():
-                assert key
-                assert not value
-
-        return device
-
-
-    '''
-    '''
-    def test_full_garbage(self):
-        """Test device initialization with a complete input consisting of
-        unicode garbage, meaning that all attributes will be filled with
-        meaningless unicode string.
-        """
-        tmp = Path(helper_.BASE + "/tests/garbage_full_config")
-        tmp.mkdir(exist_ok=True)
-
-        for info_source in device_.INFO_EXTRACTION_CONFIG:
-            filename = info_source[-1]
-            shutil.copy(FULL_DEVICE_CONFIG + "/" + filename, str(tmp) + "/" + filename)
-
-        # generate getprop
-        prop_names = ["[dalvik.vm.stack-trace-file]",
-                      "[ro.product.cpu.abi]",
-                      "[ro.product.cpu.abi2]",
-                      "[ro.product.cpu.abilist]",
-                      "[ro.product.cpu.abilist32]",
-                      "[ro.product.cpu.abilist64]",
-                      "[ro.product.cpu.abi]",
-                      "[ro.mediatek.platform]",
-                      "[ro.board.platform]",
-                      "[dalvik.vm.stack-trace-file]",
-                      "[ro.product.model]",
-                      "[ro.product.name]",
-                      "[ro.product.manufacturer]",
-                      "[ro.product.brand]",
-                      "[ro.product.device]",
-                      "[ro.build.version.release]",
-                      "[ro.build.version.sdk]",
-                      "[ro.build.id]",
-                      "[ro.build.fingerprint]",
-                      "[ro.sf.lcd_density]"
-                     ]
-
-        garbage_ = "".join(
-            [chr(x) for x in range(1, 0x100) if chr(x).isprintable()])
-
-        import string
-
-        for character in string.whitespace:
-            garbage_.replace(character, '')
-        with (tmp / "getprop").open(mode="w", encoding="utf-8") as getprop_file:
-
-            for prop in prop_names:
-                getprop_file.write(prop + " : [")
-                getprop_file.write(garbage_)
-                getprop_file.write("]\n")
-
-        device = DummyDevice(tmp)
-        for config_file in tmp.iterdir():
-            config_file.unlink()
-        tmp.rmdir()
-
-        #assert device.available_commands
-        #assert device.anr_trace_path
-
-        for category in device._info.values():
-            for key, value in category.items():
-                assert category and key and value
-
-        return device
-    '''
