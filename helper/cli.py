639ed43424b3829d307b79ef91783a43969bf650
rmmbear <rmmbear@users.noreply.github.com>
2019-03-15 16:26:37 +0100

    Refactor cli parser definition

--- helper/cli.py
+++ helper/cli.py
@@ -1,4 +1,4 @@
-""""""
+"""Command line interface module"""
 import sys
 import logging
 from pathlib import Path
@@ -13,137 +13,120 @@ LOGGER = logging.getLogger(__name__)
 HELPER_CLI_DESC = helper_.VERSION_STRING
 
 PARSER = ArgumentParser(prog="helper", description=HELPER_CLI_DESC)
-PARSER.add_argument("-v", "--version", action="version",
-                    version="".join([helper_.VERSION_STRING, " : ",
-                                     helper_.COPYRIGHT_STRING]))
+PARSER.add_argument(
+    "-v", "--version", action="version", version=helper_.VERSION_STRING)
+
 COMMANDS = PARSER.add_subparsers(title="Commands", dest="command", metavar="")
 
 ### Gneral-use optional arguments
-HELP_DEVICE = """Specify command target by passing a device's serial number.
-This value must be given if there are multiple devices connected."""
-OPT_DEV = ArgumentParser("device", add_help=False)
-OPT_DEV.add_argument("-d", "--device", default="", metavar="device",
-                     help=HELP_DEVICE)
+OPT_DEVICE = ArgumentParser("device", add_help=False)
+OPT_DEVICE.add_argument(
+    "-d", "--device", default="", metavar="device",
+    help="""Specify command target by passing a device's serial number.
+    This value must be given if there are multiple devices connected.""")
 
-HELP_OUTPUT = """Specify the output directory. If no directory is chosen, the
-files will be saved in the same directory helper was launched from."""
-OPT_OUT = ArgumentParser("output", add_help=False)
-OPT_OUT.add_argument("-o", "--output", default=".", metavar="directory",
-                     help=HELP_OUTPUT)
+OPT_OUTPUT = ArgumentParser("output", add_help=False)
+OPT_OUTPUT.add_argument(
+    "-o", "--output", default=".", metavar="directory",
+    help="""Specify the output directory. If no directory is chosen, the files
+    will be saved in the same directory helper was launched from.""")
 
 
 ### Helper Commands definitions
-HELP_INSTALL = """Install one or more apps on a device."""
-HELP_INSTALL_DETAIL = """If another version of the app being installed is
-already on the device, helper will attempt to remove it and all its data first
-before replacing them. Note that system apps can only be replaced with newer
-versions and the '--replace_system_apps' option must be specified."""
-CMD = COMMANDS.add_parser("install", parents=[OPT_DEV, OPT_OUT], aliases=["i"],
-                          help=HELP_INSTALL, epilog=HELP_INSTALL_DETAIL)
-HELP_INSTALL_FILES = """.apk file(s) or one apk and its obb expansion
-file(s)."""
-CMD.add_argument("install", nargs="+", metavar="files",
-                 help=HELP_INSTALL_FILES)
-HELP_INSTALL_KEEP = """Keep data and cache directories when replacing apps."""
-CMD.add_argument("--keep-data", action="store_true", help=HELP_INSTALL_KEEP)
-HELP_INSTALL_LOCATION = """Set the install location to either internal or
-external SD card. By default it is set to 'automatic', which lets the device
-decide the location based on available storage space and install location set
-in app's AndroidManifest.xml."""
-CMD.add_argument("--location", choices=["internal", "external"],
-                 default="automatic", help=HELP_INSTALL_LOCATION)
-HELP_INSTALL_NAME = """Use this option to set the installer name used during
-installation. By default it is 'android.helper'. Under normal circumstances
-this would be the name of the appstore app used, so for example:
-com.sec.android.app.samsungapps (Samsung Galaxy Apps), com.android.vending
-(Google Play Store), com.amazon.mShop.android (Amazon Underground),
-com.amazon.venezia (Amazon appstore--the native appstore app for Kindle Fire).
-Changing installer name may be useful for testing store-specific functionality.
-"""
-CMD.add_argument("--installer-name", default="android.helper",
-                 help=HELP_INSTALL_NAME)
+CMD = COMMANDS.add_parser(
+    "install", parents=[OPT_DEVICE, OPT_OUTPUT], aliases=["i"],
+    help="Install one or more apps on a device.",
+    epilog="""If another version of the app being installed is already on the
+    device, helper will attempt to remove it and all its data first before
+    replacing them. Note that system apps can only be replaced with newer
+    versions and the '--replace_system_apps' option must be specified.""")
 
+CMD.add_argument(
+    "install", nargs="+", metavar="files",
+    help=".apk file(s) or one apk and its obb expansion file(s).")
 
-HELP_CLEAN = """Clean various files from a device. """
-HELP_CLEAN_DETAIL = """By default, this command removes only helper-created
-files but its behavior can be customized with cleaner config file. Currently
-available options are: removing files and directories, clearing app data,
-uninstalling apps and replacing files on device with local versions. For
-configuration example, see the default config file: {}.
-""".format(helper_.CLEANER_CONFIG)
-CMD = COMMANDS.add_parser("clean", parents=[OPT_DEV, OPT_OUT], aliases="c",
-                          help=HELP_CLEAN, epilog=HELP_CLEAN_DETAIL)
-HELP_CLEAN_CONFIG = """Path to a valid cleaner config file. For example of a
-valid config, see the default file in this program's root directory."""
-CMD.add_argument("clean", nargs="?", default=helper_.CLEANER_CONFIG,
-                 metavar="config", help=HELP_CLEAN_CONFIG)
+CMD.add_argument(
+    "--keep-data", action="store_true",
+    help="Keep data and cache directories when replacing apps.")
 
+CMD.add_argument(
+    "--location", choices=["internal", "external"], default="automatic",
+    help="""Set the install location to either internal or external SD card. By
+    default it is set to 'automatic', which lets the device decide the location
+    based on available storage space and install location set in app's
+    AndroidManifest.xml.""")
 
-HELP_RECORD = """Record the screen of your device."""
-HELP_RECORD_DETAIL = """To stop and save the recorded video, press 'ctrl+c'.
-Videos have a hard time-limit of three minutes--this is imposed by the internal
-command and cannot be extended--recording will be stopped automatically after
-reaching that limit. NOTE: Sound is not, and cannot be recorded."""
-CMD = COMMANDS.add_parser("record", parents=[OPT_DEV, OPT_OUT], aliases="r",
-                          help=HELP_RECORD, epilog=HELP_RECORD_DETAIL)
+CMD.add_argument(
+    "--installer-name", default="android.helper",
+    help="""Use this option to set the installer name used during installation.
+    By default it is 'android.helper'. Under normal circumstances this would be
+    the name of the appstore app used, so for example:
+    com.sec.android.app.samsungapps (Samsung Galaxy Apps),
+    com.android.vending (Google Play Store),
+    com.amazon.mShop.android (Amazon Underground - Android app),
+    com.amazon.venezia (Amazon appstore - native Kindle Fire).
+    Changing installer name may be useful for testing store-specific
+    functionality.""")
 
+CMD = COMMANDS.add_parser(
+    "clean", parents=[OPT_DEVICE, OPT_OUTPUT], aliases="c",
+    help="Clean the device storage as per the instructions in cleaner config.",
+    epilog=f"""By default, this command removes only helper-created
+    files but its behavior can be customized with cleaner config file.
+    Currently available options are: removing files and directories, clearing
+    app data, uninstalling apps and replacing files on device with local
+    versions. For configuration example, see the default config file:
+    {helper_.CLEANER_CONFIG}.""")
 
-# TODO: Write a more detailed description of the traces function
-# It's pretty straightforward, so I don't know if there's anything else
-# worth mentioning?
-HELP_TRACES = """Save the dalvik vm stack traces (aka ANR log) to a file."""
-CMD = COMMANDS.add_parser("traces", parents=[OPT_DEV, OPT_OUT], aliases="t",
-                          help=HELP_TRACES, epilog=HELP_TRACES)
+CMD.add_argument(
+    "clean", nargs="?", default=helper_.CLEANER_CONFIG, metavar="config",
+    help="""Path to a valid cleaner config file. For example of a
+    valid config, see the default file in this program's root directory.""")
 
+CMD = COMMANDS.add_parser(
+    "record", parents=[OPT_DEVICE, OPT_OUTPUT], aliases="r",
+    help="Record the screen of your device.",
+    epilog="""To stop and save the recorded video, press 'ctrl+c'.
+    Videos have a hard time-limit of three minutes -- this is imposed by
+    the internal command and cannot be extended -- recording will be stopped
+    automatically after reaching that limit. NOTE: Sound is not recorded.""")
 
-HELP_EXTRACT = """Extract the .apk file of an installed application."""
+CMD = COMMANDS.add_parser(
+    "traces", parents=[OPT_DEVICE, OPT_OUTPUT], aliases="t",
+    help="Save the dalvik vm stack traces (aka ANR log) to a file.",
+    epilog="Save the dalvik vm stack traces (aka ANR log) to a file.")
+
 # TODO: Update detailed description after implementing obb extraction
-HELP_EXTRACT_DETAIL = """Extract the .apk file from device's storage. On some
-devices the archives cannot be extracted. In general, if it is possible to
-extract third part apps, it should be also possible to the same with system
-apps. Note: app's expansion files cannot yet be extracted."""
-CMD = COMMANDS.add_parser("extract-apk", parents=[OPT_DEV, OPT_OUT],
-                          aliases="x", help=HELP_EXTRACT,
-                          epilog=HELP_EXTRACT_DETAIL)
-HELP_EXTRACT_NAME = """Package ID of an installed app. For example:
-android.com.browser."""
-CMD.add_argument("extract_apk", nargs="+", metavar="app name",
-                 help=HELP_EXTRACT_NAME)
+CMD = COMMANDS.add_parser(
+    "extract-apk", parents=[OPT_DEVICE, OPT_OUTPUT], aliases="x",
+    help="""Extract the .apk file of an installed application.""",
+    epilog="""Extract the .apk file from device's storage. On some devices
+    the archives cannot be extracted. In general, if it is possible to
+    extract third part apps, it should be also possible to the same with
+    system apps. Note: app's expansion files (OBBs) cannot yet be extracted.""")
 
+CMD.add_argument(
+    "extract_apk", nargs="+", metavar="app name",
+    help="Package ID of an installed app. For example: android.com.browser.")
 
-HELP_SCAN = """Show status of all connected devices."""
-HELP_SCAN_DETAIL = """Scan shows serial number, manufacturer, model and
-connection status of all devices connected. If a connection with device could
-not be established, only its serial and connection status is shown."""
-CMD = COMMANDS.add_parser("scan", aliases="s", help=HELP_SCAN,
-                          epilog=HELP_SCAN_DETAIL)
+COMMANDS.add_parser(
+    "scan", aliases="s",
+    help="Show status of all connected devices.",
+    epilog="""Scan shows serial number, manufacturer, model and connection
+    status of all devices connected. If a connection with device could not
+    be established, only its serial and connection status is shown.""")
 
-
-#HELP_DSCAN = """Show status and detailed information of connected devices."""
-#HELP_DSCAN_DETAIL = """Detailed scan displays all info from normal scan as well as
-#amount of available RAM, version of the OS and basic specifications of the GPU,
-#CPU and display."""
-#CMD = COMMANDS.add_parser("detailed-scan", parents=[OPT_DEV, OPT_OUT],
-#                          aliases=["ds"], help=HELP_DSCAN,
-#                          epilog=HELP_DSCAN_DETAIL)
-
-# TODO: Write a more detailed description of the dump function
-# It's pretty straightforward, so I don't know if there's anything else
-# worth mentioning?
-HELP_DUMP = """Dump all available device information to file."""
-CMD = COMMANDS.add_parser("dump", aliases=["d"], parents=[OPT_DEV, OPT_OUT],
-                          help=HELP_DUMP, epilog=HELP_DUMP)
+COMMANDS.add_parser(
+    "dump", aliases=["d"], parents=[OPT_DEVICE, OPT_OUTPUT],
+    help="Dump all available device information to file.",
+    epilog="Dump all available device information to file.")
 
 ### Hidden commands
 #COMMANDS.add_parser("gui")
-CMD = COMMANDS.add_parser("debug-dump", parents=[OPT_DEV, OPT_OUT])
+CMD = COMMANDS.add_parser("debug-dump", parents=[OPT_DEVICE, OPT_OUTPUT])
 CMD.add_argument("--full", action="store_true")
 CMD = COMMANDS.add_parser("run-tests")
 
-# Example of caring too much about aesthetics
-for group in  PARSER._action_groups:
-    if group.title == 'optional arguments':
-        group.title = None
 
 PARSER_NO_ARGS = PARSER.parse_args([])
 
@@ -341,7 +324,7 @@ def main(args=None):
         BATCH_COMMANDS[args.command](chosen_device, args)
         return
 
-    if args.command in ("run-tests"):
+    if args.command == "run-tests":
         run_tests()
         return
 
