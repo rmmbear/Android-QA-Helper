5ed158ec38e11de4b6dda817f1d978b3d48172d6
rmmbear <rmmbear@users.noreply.github.com>
2019-07-21 01:09:17 +0200

    Create a very bare-bones cx_freeze setup script

---
+++ cxfreeze_setup.py
@@ -0,0 +1,25 @@
+import sys
+from cx_Freeze import setup, Executable
+
+from helper import VERSION
+
+options = {
+    "build_exe": {
+        "excludes": ["tkinter"],
+        "optimize": 2,
+        "path": sys.path + ["helper", "helper/tools"]
+    },
+}
+
+executables = (
+    Executable("./helper/tools/tool_grabber.py"),
+    Executable("./helper/apk.py", targetName="app_inspector"),
+    Executable("./helper/__main__.py", targetName="helper"),
+)
+
+setup(
+    name="Android QA Helper",
+    version=VERSION,
+    options=options,
+    executables=executables
+)
